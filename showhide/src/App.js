d04984f83fa508e3135ef86d1d37045d5e29a924
Vijay Thirugnanam <vijayst@Vijays-MacBook-Air.local>
2018-01-28 19:49:10 +0530

    changed import statement

--- showhide/src/App.js
+++ showhide/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Transition from 'react-transition-group/Transition';
+import { Transition } from 'react-transition-group';
 
 class App extends Component {
     state = { show: false };
